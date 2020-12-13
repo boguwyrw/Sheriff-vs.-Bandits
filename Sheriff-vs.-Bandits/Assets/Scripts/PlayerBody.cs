8686afc2845ce033e1e8ed42c5bbf6f49b2f27b2
boguwyrw <b.wyrwal@wp.pl>
2020-12-13 15:06:17 +0100

    Player movement was finish

---
+++ Sheriff-vs.-Bandits/Assets/Scripts/PlayerBody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBody : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick horizontalJoystick = null;
+
+    private float horizontalMove = 0.0f;
+    private float playerSpeed = 0.0f;
+    private float speed = 12.0f;
+
+    private void Start()
+    {
+        playerSpeed = speed;
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalMove = horizontalJoystick.Horizontal * playerSpeed * Time.deltaTime;
+        transform.parent.Translate(horizontalMove, 0, 0);
+    }
+}
---
+++ Sheriff-vs.-Bandits/Assets/Scripts/PlayerHead.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHead : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick allAxisJoystick = null;
+
+    private Vector2 direction;
+    private Transform playerBody;
+    private float horizontalRotation = 0.0f;
+    private float verticalRotation = 0.0f;
+    private float rotationLimit = 80.0f;
+    private float headMovementSpeed = 55.0f;
+
+    private void Start()
+    {
+        playerBody = transform.parent;
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalRotation = allAxisJoystick.Horizontal;
+        verticalRotation = allAxisJoystick.Vertical;
+
+        Vector2 changesPosition = new Vector2(horizontalRotation, verticalRotation);
+        direction += changesPosition;
+
+        direction.y = Mathf.Clamp(direction.y, -rotationLimit, rotationLimit);
+        transform.localRotation = Quaternion.AngleAxis(-direction.y * headMovementSpeed * Time.deltaTime, Vector3.right);
+        playerBody.localRotation = Quaternion.AngleAxis(direction.x * headMovementSpeed * Time.deltaTime, Vector3.up);
+    }
+}
