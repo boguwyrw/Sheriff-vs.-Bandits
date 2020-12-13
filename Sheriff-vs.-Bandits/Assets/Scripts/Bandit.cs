a2b0a13ee8deb5665989f6d57a438944f034ca5b
boguwyrw <b.wyrwal@wp.pl>
2020-12-13 20:27:17 +0100

    Sounds have been added

---
+++ Sheriff-vs.-Bandits/Assets/Scripts/Bandit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bandit : MonoBehaviour
+{
+    private bool banditIsDying = false;
+    private AudioSource banditAudioSource;
+
+    private void Start()
+    {
+        banditAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (banditIsDying)
+        {
+            Quaternion onGround = Quaternion.Euler(-90.0f, 180.0f, 0.0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, onGround, 5 * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            banditAudioSource.Play();
+            banditIsDying = true;
+        }
+    }
+}
---
+++ Sheriff-vs.-Bandits/Assets/Scripts/ShootButton.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootButton : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private RectTransform background = null;
+    [SerializeField]
+    private Rigidbody bulletPrefab = null;
+    [SerializeField]
+    private Transform shootPoint = null;
+
+    private Vector3 target;
+    private float playerTime = 0.5f;
+    private Vector3 bulletVelocity;
+    private Vector3 result;
+    private Rigidbody clone;
+    private RaycastHit raycastHit;
+    private float range = 50.0f;
+    private AudioSource revolverAudioSource;
+
+    private void Start()
+    {
+        revolverAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        clone = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+        clone.velocity = bulletVelocity;
+        revolverAudioSource.Play();
+    }
+
+    private void Update()
+    {
+        Shooting();
+    }
+
+    private void Shooting()
+    {
+        Vector3 centerPoint = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+        if (Physics.Raycast(centerPoint, Camera.main.transform.forward, out raycastHit, range))
+        {
+            target = raycastHit.point;
+        }
+        else
+        {
+            target = centerPoint + Camera.main.transform.forward * range;
+        }
+
+        bulletVelocity = CalculateVelocity(target, shootPoint.position, playerTime);
+    }
+
+    private Vector3 CalculateVelocity(Vector3 endPoint, Vector3 startPoint, float ballTime)
+    {
+        Vector3 vectorDistance = endPoint - startPoint;
+        Vector3 vectorDistanceXZ = vectorDistance;
+        vectorDistanceXZ.y = 0;
+
+        float distanceY = vectorDistance.y;
+        float distanceXZ = vectorDistanceXZ.magnitude;
+
+        float velocityXZ = distanceXZ / ballTime;
+        float velocityY = distanceY / ballTime + 0.5f * Mathf.Abs(Physics.gravity.y) * ballTime;
+
+        result = vectorDistanceXZ.normalized;
+        result *= velocityXZ;
+        result.y = velocityY;
+
+        return result;
+    }
+}
